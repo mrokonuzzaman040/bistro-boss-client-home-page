7645a012ce1596a5676b1e3eb1439622a7b23e62
mrokonuzzaman040 <19201103040@cse.bubt.edu.bd>
2023-11-12 02:26:57 +0600

    Refactor menu page and add max-width to main app container

--- src/main.jsx
+++ src/main.jsx
@@ -8,10 +8,12 @@ import { router } from './Routes/Routes';
 import { HelmetProvider } from 'react-helmet-async';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <HelmetProvider>
-      <RouterProvider router={router}>
-      </RouterProvider>
-    </HelmetProvider>
-  </React.StrictMode>,
+  <div className='max-w-screen-xl mx-auto'>
+    <React.StrictMode>
+      <HelmetProvider>
+        <RouterProvider router={router}>
+        </RouterProvider>
+      </HelmetProvider>
+    </React.StrictMode>,
+  </div>
 )
--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -1,14 +1,16 @@
 import { Helmet } from "react-helmet-async";
 import Cover from "../Shared/Cover/Cover";
 import img from "../../assets/menu/banner3.jpg"
+import PopularMenu from "../Home/PopularMenu/PopularMenu";
 
 const Menu = () => {
     return (
-        <div>
+        <div className="">
             <Helmet>
                 <title>Bistro Boss || Menu</title>
             </Helmet>
-            <Cover img={img}></Cover>
+            <Cover img={img} titel={"Our Menu"}></Cover>
+            <PopularMenu></PopularMenu>
         </div>
     );
 };
